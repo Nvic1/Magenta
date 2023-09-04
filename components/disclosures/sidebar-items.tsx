d03d0a3b9d39417151bdcb9dd4cc0207de49d70a
Nvic1 <denfu.oscar.dev@gmail.com>
2023-09-04 13:45:27 +0000

    add components/ folder

---
+++ components/disclosures/sidebar-items.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { Disclosure, Transition } from '@headlessui/react'
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import NavItem from '../miscellaneous/dashboard-link';
+
+const menuItems = [
+
+  {
+    name: 'My Catalogue', 
+    props: {
+      urlOne: `/bookstore/catalogue/books`, 
+      urlTwo: `/bookstore/catalogue/new`, 
+      one:`My Books`, 
+      two: `Add New Book`,
+    }
+  },
+
+
+]
+
+
+export default function NavItemDrop() {
+  return (
+<>
+
+  {
+    menuItems.map(item => (
+
+
+      <Disclosure>
+        {({ open }) => (
+          <li>
+            <Disclosure.Button className="pl-2 flex space-x-4 text-left text-base w-full hover:text-cyan-700">
+              <ChevronRightIcon
+                className={`${
+                  open ? 'rotate-90 transform text-cyan-800' : 'text-slate-700'
+                } h-5 w-5  `}
+              />
+              
+              <span
+                  className={`text-base   ${
+                    open ? 'text-cyan-800 font-medium': 'text-slate-700'
+                  } hover:text-cyan-700`}
+              >{item.name}</span>                
+
+            </Disclosure.Button>
+            <div className="flex flex-col space-y-4 ml-11">
+
+              <Disclosure.Panel className="text-left text-base text-slate-700 pt-6">  
+                <NavItem href={item.props?.urlOne}>{item.props?.one}</NavItem>
+              </Disclosure.Panel>
+              <Disclosure.Panel className="text-left text-base text-slate-700">  
+                <NavItem href={item.props?.urlTwo}>{item.props?.two}</NavItem>
+              </Disclosure.Panel>
+            </div>
+
+          </li>
+        )}
+      </Disclosure>
+  ))}
+</>
+
+  )
+}
